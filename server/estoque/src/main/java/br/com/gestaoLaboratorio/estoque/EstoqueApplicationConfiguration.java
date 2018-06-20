edce39212d3b4a2c16c8247958b0d9b5c28aa452
Ednezer.Rezende <Ednezer.Rezende@infosolo.com.br>
2018-06-20 11:45:26 -0300

    Server

---
+++ server/estoque/src/main/java/br/com/gestaoLaboratorio/estoque/EstoqueApplicationConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.gestaoLaboratorio.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value = "/")
+public class EstoqueApplicationConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueApplicationConfiguration.class, args);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String teste(){
+		return "Olá Ednezer";
+	}
+}
---
+++ server/estoque/src/test/java/br/com/gestaoLaboratorio/estoque/EstoqueApplicationConfigurationTests.java
@@ -0,0 +1,16 @@
+package br.com.gestaoLaboratorio.estoque;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstoqueApplicationConfigurationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
