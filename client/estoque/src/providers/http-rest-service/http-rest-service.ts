318ab58f28a719cc8bc229d1986efae7b4276110
Ednezer.Rezende <Ednezer.Rezende@infosolo.com.br>
2018-07-03 15:32:10 -0300

    Adaptando http_rest

--- client/estoque/src/providers/http-rest-service/http-rest-service.ts
+++ client/estoque/src/providers/http-rest-service/http-rest-service.ts
@@ -1,11 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HttpRestServiceProvider {
 
   private _url = 'http://192.168.1.193:8080/api/';
+  public log:any;
+  private _headers = new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8'
+  ,'Access-Control-Allow-Origin': '*'});
 
   constructor(private _http: HttpClient) {
 
@@ -14,5 +17,22 @@ export class HttpRestServiceProvider {
   getUrl(){
     return this._url;
   }
+
+  getHeaders(){
+    return this._headers;
+  }
+
+  public handleAngularJsonBug (error: HttpErrorResponse) {
+		const JsonParseError = 'Http failure during parsing for';
+		const matches = error.message.match(new RegExp(JsonParseError, 'ig'));
+
+		if (error.status === 200 && matches.length === 1) {
+      // return obs that completes;
+			return Observable;
+		} else {
+			this.log.debug('re-throwing ');
+			return Observable.throw(error);	
+		}
+	}
 
 }
--- client/estoque/src/providers/produto-service/produto-service.ts
+++ client/estoque/src/providers/produto-service/produto-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+
 import { Produto } from '../../modelos/produtos';
 import { HttpRestServiceProvider } from '../http-rest-service/http-rest-service';
 
@@ -9,15 +9,13 @@ import { HttpRestServiceProvider } from '../http-rest-service/http-rest-service'
 export class ProdutoServiceProvider {
 
   private _url:string;
-  
-  public log:any;
-  public handleError:any;
 
-  private _headers = new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8'
-  ,'Access-Control-Allow-Origin': '*'});
+  public handleError:any;
+  private _headers:HttpHeaders;
 
   constructor(private _http: HttpClient, private _httpRest: HttpRestServiceProvider) {
     this._url = this._httpRest.getUrl();
+    this._headers = this._httpRest.getHeaders();
   }
 
   salvar(produto: Produto){
@@ -32,19 +30,8 @@ export class ProdutoServiceProvider {
     return this._http.delete(this._url + `produto/excluirProduto/${id}`, {
       headers: this._headers
     })
-    .catch((error: HttpErrorResponse) => this.handleAngularJsonBug(error));
+    .catch((error: HttpErrorResponse) => this._httpRest.handleAngularJsonBug(error));
   }        
 
-  private handleAngularJsonBug (error: HttpErrorResponse) {
-		const JsonParseError = 'Http failure during parsing for';
-		const matches = error.message.match(new RegExp(JsonParseError, 'ig'));
-
-		if (error.status === 200 && matches.length === 1) {
-      // return obs that completes;
-			return Observable;
-		} else {
-			this.log.debug('re-throwing ');
-			return Observable.throw(error);		// re-throw
-		}
-	}
+  
 }
